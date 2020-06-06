4a3c8200d731fb16f8065105d15c3ac71c2d9c3a
dawiddd95 <dawlyc@wp.pl>
2020-06-06 15:40:16 +0200

    defining relations between models

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,2 +1,4 @@
 class Reply < ApplicationRecord
+   belongs_to :discussion
+   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200606133007) do
+ActiveRecord::Schema.define(version: 20200606133305) do
+
+  create_table "channels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "channel"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "discussion_id"
+  end
 
   create_table "discussions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "channel_id"
+  end
+
+  create_table "replies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text "reply"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "discussion_id"
     t.integer "user_id"
   end
 
